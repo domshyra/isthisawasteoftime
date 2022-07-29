ab2e3d3efc55a7d5c581f12e218cbc7528f39a55
domshyra <domshyra@gmail.com>
2022-07-29 15:47:58 -0700

    Added an app bar

--- WebApp/src/components/shared/Layout.jsx
+++ WebApp/src/components/shared/Layout.jsx
@@ -1,10 +1,14 @@
 import Container from '@mui/material/Container';
+import MyAppBar from '../MyAppBar';
 import React from 'react';
 
 export const Layout = (props) => {
     return (
+        <>
+        <MyAppBar />
         <Container>
             {props.children}
         </Container>
+        </>
     );
 };
---
+++ WebApp/src/configs/constants.js
@@ -0,0 +1,24 @@
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+import React from 'react';
+
+const primaryKey = 'Id';
+
+export const application = {
+    label: "Music challenges app",
+
+}
+
+
+const challengesRoot = '/Challenges';
+const challengesApiPath = 'api/Challenges';
+const challengesItemLabel = 'Challenge';
+const challengesLabel = `${challengesItemLabel}s`;
+
+export const challenges = {
+    label: challengesLabel,
+    itemLabel: challengesLabel,
+    path: challengesRoot,
+    apiPath: challengesApiPath,
+    primaryKey: primaryKey,
+    icon: <MilitaryTechIcon />
+};
