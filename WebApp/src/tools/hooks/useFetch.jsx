17e14b7630ec897ed60ba315dfdc36e487b333a5
Dominic Schira <domshyra@gmail.com>
2022-10-12 13:15:47 -0700

    Update useFetch.jsx
    added about controler

--- WebApp/src/tools/hooks/useFetch.jsx
+++ WebApp/src/tools/hooks/useFetch.jsx
@@ -10,6 +10,8 @@ import config from "../../configs/config";
  */
 export default function useFetch(url, defaultObject = {}) {
 	const isMountedRef = useRef(false);
+	const controller = new AbortController();
+	const signal = controller.signal;
 	const [loaded, setLoaded] = useState(false);
 	const [error, setError] = useState(null);
 	const [data, setData] = useState(null);
@@ -17,7 +19,15 @@ export default function useFetch(url, defaultObject = {}) {
 	useEffect(() => {
 		isMountedRef.current = true;
 		const fetchUrl = `${config.ApplicationServiceUri}${url}`;
-		fetch(fetchUrl)
+		fetch(fetchUrl, {
+			method: "GET",
+			headers: {
+				"Accept": "application/x-www-form-urlencoded",
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			mode: "cors",
+			signal: signal,
+		})
 			.then((response) => {
 				if (!response.ok) {
 					let error = response.text();
@@ -30,14 +40,23 @@ export default function useFetch(url, defaultObject = {}) {
 			})
 			.then(
 				(result) => {
-					if (isMountedRef.current) setData(result);
+					if (isMountedRef.current) {
+						if (Object.keys(result).length) {
+							setData(result);
+						} else {
+							setData(defaultObject);
+						}
+					}
 				},
 				(error) => {
 					if (isMountedRef.current) setError(error);
 				}
 			)
 			.catch((e) => {
-				if (isMountedRef.current) setError(e);
+				if (isMountedRef.current) {
+					console.error(`Download error: ${e.message}`);
+					setError(e);
+				}
 			})
 			.finally(() => {
 				if (isMountedRef.current) setLoaded(true);
@@ -45,6 +64,7 @@ export default function useFetch(url, defaultObject = {}) {
 
 		//If the caller gets unmounted then we don't waste react resources
 		return () => {
+			controller.abort();
 			isMountedRef.current = false;
 		};
 	}, [url]);
